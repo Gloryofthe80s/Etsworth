8a6d38c837f03bcdc478bb91f55f005dbc4a63f6
Gloryofthe80s <gloryoftheeighties@gmail.com>
2014-03-17 15:10:42 -0400

    initialized

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Etsworth, $*/
+
+
+window.Etsworth = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Etsworth.init();
+});
